3c24a5465e3b7f5daa0bbf1916f4178470e9883f
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-12-12 15:54:21 -0500

    final product

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i = i - 1
+    print('Happy New Year!')
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+def square_integers(int_list = [1, 2, 3, 4, 5]):
+    square_integer = [i**2 for i in int_list]
+    return square_integer
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i <= 100:
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else: 
+            print(i)
+        i = i + 1
+
+print(fizzbuzz())
